8ba4c55a1ae958e5ce28195d8e660e8f60c768c6
YtBelll <ytbell@outlook.com>
2023-09-05 10:01:10 -0300

    Create Dicas.java
    model Dicas

---
+++ ProjetoBase/ProjetoBase/src/br/com/ifba/dicas/model/Dicas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.dicas.model;
+
+/**
+ *
+ * @author Heber
+ */
+public class Dicas {
+    private int id;
+    private String dica;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDica() {
+        return dica;
+    }
+
+    public void setDica(String dica) {
+        this.dica = dica;
+    }
+    
+    
+}
